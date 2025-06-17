58e5d2d939e4397307a0c6a0a9b815a528ccf02b
De la Fuente De la Peña <juan.de10@isepceu.es>
2025-06-17 17:42:16 +0200

    Modificación e inclusion perímetro en clase Principal Juan_Alumno

--- Principal_Juan.java
+++ Principal_Juan.java
@@ -3,6 +3,11 @@ package ordina;
 public class Principal_Juan {
 	
 	public static void main(String[] args) {
+		
+		//crear permitero de circulo y rectangulo
+		double perimetroCirculo = calcularPerimetroCirculo(3);
+        double perimetroRectangulo = calcularPerimetroRectangulo(3, 3);
+        
 		//instanciamos un cuadrado y un circulo
 		Delafuente_Circulo circulo1 = new Delafuente_Circulo(3,"Verde");
 		Delafuente_Rectangulo rectangulo1 = new Delafuente_Rectangulo(3,3, "Rojo");
@@ -18,6 +23,8 @@ public class Principal_Juan {
 		double area1 = radio*3.14;
 		System.out.println("Area circulo: "+area1);
 		
+		//
+		
 		//diferencia entre area de circulo y rectangulo
 		double diferencia = area-area1;
 		System.out.println("Diferencia areas "+diferencia);
@@ -26,8 +33,21 @@ public class Principal_Juan {
 		System.out.println("cuadrado: "+rectangulo1);
 		System.out.println("circulo: "+circulo1);
 		System.out.println("diferencia: "+diferencia);
+		System.out.println("Perímetro del círculo: " + perimetroCirculo);
+        System.out.println("Perímetro del rectángulo: " + perimetroRectangulo);
 		//////
-		
+	      
+
+
+	    }
+
+	    private static double calcularPerimetroCirculo(double radio) {
+	        return 2 * Math.PI * radio;
+	    }
+
+	    private static double calcularPerimetroRectangulo(double ladoA, double ladoB) {
+	        return (2 * ladoA) + (2 * ladoB);
+	    }
 	}
-}
+
 
